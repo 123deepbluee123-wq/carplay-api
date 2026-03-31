0a0e6f7a3d31568105972900ebfcbac5a118cda9
123deepbluee123-wq <123deepbluee123@gmail.com>
2026-03-31 10:28:49 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,1 +1,12 @@
+<?php
+header('Content-Type: application/json');
 
+echo json_encode([
+    "status" => "success",
+    "license_active" => true,
+    "trial_active" => false,
+    "message" => "Sınırsız Lisans Aktif",
+    "remaining_hours" => 9999,
+    "remaining_minutes" => 59
+]);
+?>
